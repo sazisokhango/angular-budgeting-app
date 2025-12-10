53470f6e4bdef509f5e5a6c1cd7dea5b806cd442
sazisokhango <101212335+sazisokhango@users.noreply.github.com>
2025-12-10 15:17:03 +0200

    Adding a dashboard and components needed (#1)
    Co-authored-by: saziso <saziso.khango@thumbtribe.mobi>

---
+++ src/app/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
