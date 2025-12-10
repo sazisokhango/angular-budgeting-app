0876295d9f6e5b3a295284b0ffebf5ed5e25606e
saziso <saziso.khango@thumbtribe.mobi>
2025-12-10 15:13:15 +0200

    Adding a dashboard and components needed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Transactions } from './transactions/transactions';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Dashboard,
+        children: [
+            {
+                path: 'home',
+                component: Home
+            },
+            {
+                path: 'transactions',
+                component: Transactions
+            }
+        ]
+
+    },
+    
+];
