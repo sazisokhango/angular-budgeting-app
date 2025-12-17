7b2899bfb0ab71d2481087152f4c4c6b1f1d792a
saziso <saziso.khango@thumbtribe.mobi>
2025-12-17 12:05:27 +0200

    changing component names

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,20 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { Transactions } from './transactions/transactions';
-import { Dashboard } from './dashboard/dashboard';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: Dashboard,
+        component: DashboardComponent,
         children: [
             {
                 path: 'home',
-                component: Home
+                component: HomeComponent
             },
             {
                 path: 'transactions',
-                component: Transactions
+                component: TransactionsComponent
             }
         ]
 
---
+++ src/app/model/transaction.model.ts
@@ -0,0 +1,17 @@
+import { BudgetModel } from "./budget.model";
+import { CategoryModel } from "./category.model";
+import { ScheduleModel } from "./schedule.model";
+
+export interface TransactionModel {
+      occurredAt: Date,
+      amount: number,
+      reference: string,
+      description: string
+      category: CategoryModel,
+      budget: BudgetModel,
+      schedule: ScheduleModel,
+      createdBy: String,
+      createdAt: Date,
+      updatedBy: string,
+      updatedAt: Date,
+}
