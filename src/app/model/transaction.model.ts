a922335932004387112554753a72e2d0ba79a870
sazisokhango <101212335+sazisokhango@users.noreply.github.com>
2026-01-09 08:10:00 +0200

    Add new transaction (#5)
    * Adding a Toast and service for it
    
    * Adding a drawer
    
    * Adding models
    
    * Adding more api calls for budget, categories and new transaction
    
    * creating a new transaction
    
    ---------
    
    Co-authored-by: saziso <saziso.khango@thumbtribe.mobi>

--- src/app/model/transaction.model.ts
+++ src/app/model/transaction.model.ts
@@ -1,18 +1,15 @@
-import { BudgetModel } from "./budget.model";
-import { CategoryModel } from "./category.model";
-import { ScheduleModel } from "./schedule.model";
-
+import { BudgetModel } from './budget.model';
+import { CategoryModel } from './category.model';
 
 export interface TransactionModel {
-      occurredAt: Date | null,
-      amount: number,
-      reference: string,
-      description: string
-      category: CategoryModel,
-      budget: BudgetModel,
-      schedule: ScheduleModel,
-      createdBy: String,
-      createdAt: Date,
-      updatedBy: string,
-      updatedAt: Date,
+  occurredAt: Date | null;
+  amount: number;
+  reference: string;
+  description: string;
+  category: CategoryModel;
+  budget: BudgetModel;
+  createdBy: String;
+  createdAt: Date;
+  updatedBy: string;
+  updatedAt: Date;
 }
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -2,22 +2,36 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TransactionService } from '../service/transaction.service';
 import { TransactionTableComponent } from '../transaction-table/transaction-table.component';
 import { TransactionModel } from '../model/transaction.model';
+import { Drawer } from '../shared/drawer/drawer.component';
+import { NewTransactionComponent } from './new-transaction.component';
+import { ToastService } from '../shared/toast.service';
 
 @Component({
   selector: 'app-transactions',
-  imports: [ TransactionTableComponent],
+  imports: [TransactionTableComponent, Drawer, NewTransactionComponent],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css',
 })
 export class TransactionsComponent implements OnInit {
   transactionService = inject(TransactionService);
+  toastService = inject(ToastService);
+  isDrawerOpen = signal(false);
 
   transactions = signal<TransactionModel[]>([]);
 
   ngOnInit(): void {
+    this.refreshTransaction();
+  }
+
+  refreshTransaction() {
     this.transactionService.getTransactions().subscribe({
       next: (data) => this.transactions.set(data),
-      error: (error) => console.log(error),
+      error: (error) => {
+        console.log(error), this.toastService.showError('Error fetching the Transactions');
+      },
     });
+  }
+  onOpen() {
+    this.isDrawerOpen.set(!this.isDrawerOpen());
   }
 }
