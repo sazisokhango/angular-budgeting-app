81cf5e23094dd10a379a07a64f24293e8328b08e
saziso <saziso.khango@thumbtribe.mobi>
2026-01-30 16:38:57 +0200

    Adding transaction Id

--- src/app/core/models/transaction.model.ts
+++ src/app/core/models/transaction.model.ts
@@ -2,13 +2,14 @@ import { BudgetModel } from './budget.model';
 import { CategoryModel } from './category.model';
 
 export interface TransactionModel {
-  occurredAt: Date | null;
+  transactionId: string
+  occurredAt: Date;
   amount: number;
   reference: string;
   description: string;
   category: CategoryModel;
   budget: BudgetModel;
-  createdBy: String;
+  createdBy: string;
   createdAt: Date;
   updatedBy: string;
   updatedAt: Date;
