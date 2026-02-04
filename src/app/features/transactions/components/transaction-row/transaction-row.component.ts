79314a4157cba75c1897fa73d72e34a61e0f9c5a
saziso <saziso.khango@thumbtribe.mobi>
2026-02-04 10:25:57 +0200

    adding Icons in the table and emitting an output

--- src/app/features/transactions/components/transaction-row/transaction-row.component.ts
+++ src/app/features/transactions/components/transaction-row/transaction-row.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, input, output } from '@angular/core';
 import { TransactionModel } from '@/app/core/models';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: '[appTransactionRow]',
@@ -14,9 +14,14 @@ import { faEdit } from '@fortawesome/free-solid-svg-icons';
 export class TransactionRowComponent {
   public readonly transaction = input.required<TransactionModel>();
   public readonly editTransaction = output<TransactionModel>();
+  public readonly deleteTransaction = output<TransactionModel>();
   protected readonly faEditIcon = faEdit;
+  protected readonly faTrashIcon = faTrash;
 
   public edit() {
     this.editTransaction.emit(this.transaction());
+  }
+  public delete() {
+    this.deleteTransaction.emit(this.transaction());
   }
 }
