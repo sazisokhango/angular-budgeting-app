21f282c9c5e05d4d515d958b43acefb4403eda8f
saziso <saziso.khango@thumbtribe.mobi>
2026-02-04 10:29:33 +0200

    Adding button color class and action method for output

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,14 +1,29 @@
-import { Component, input, output } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css',
 })
 export class ButtonComponent {
   readonly label = input.required<string>();
+  readonly buttonType = input.required<'success' | 'info' | 'danger'>();
   readonly isLoading = input<boolean>(false);
   readonly isDisabled = input<boolean>(false);
   readonly type = input<'reset' | 'submit' | 'button'>('button');
+  readonly action = output();
+
+  protected readonly buttonClass = computed(() => ({
+    'bg-red-500 hover:bg-red-700': this.buttonType() === 'danger',
+    'bg-sky-600': this.buttonType() === 'success',
+    'bg-white-600': this.buttonType() === 'info',
+  }));
+
+  actionMethod() {
+    if(this.type() !== 'submit'){
+      this.action.emit();
+    }
+  }
 }
