e6c97904056dbf247d6b3f0f4707eb576984b205
saziso <saziso.khango@thumbtribe.mobi>
2026-02-04 10:28:43 +0200

    Adding remaining coponents in a barrel file

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,2 +1,4 @@
 export * from './components/drawer/drawer.component';
 export * from './components/toast/toast.component';
+export * from './components/button/button.component';
+export * from './components/confirm-dialog/confirm-dialog.component';
