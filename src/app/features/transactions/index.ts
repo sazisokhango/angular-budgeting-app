4f27b41d100e4ee2ca160ce9416d6eadef70b242
saziso <saziso.khango@thumbtribe.mobi>
2026-02-12 11:38:45 +0200

    Adding edit-transaction to barrel file

--- src/app/features/transactions/index.ts
+++ src/app/features/transactions/index.ts
@@ -1,4 +1,5 @@
 export * from './pages/transaction-page/transaction-page.component';
 export * from './pages/new-transaction/new-transaction.component';
+export * from './pages/edit-transaction/edit-transaction.component';
 export * from './components/transaction-table/transaction-table.component';
 export * from './components/transaction-row/transaction-row.component';
