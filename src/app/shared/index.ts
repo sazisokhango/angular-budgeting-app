c2904dae8fbe202756e8c9acf8ae8af3dc7426b7
saziso <saziso.khango@thumbtribe.mobi>
2026-02-24 16:04:26 +0200

    Adding chart to barrel file

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -2,3 +2,4 @@ export * from './components/drawer/drawer.component';
 export * from './components/toast/toast.component';
 export * from './components/button/button.component';
 export * from './components/confirm-dialog/confirm-dialog.component';
+export * from './components/chart/chart.component';
