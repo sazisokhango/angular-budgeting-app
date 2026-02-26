8a9c6d6730d6affd106ec26d9d50aa0a9e3e66f8
saziso <saziso.khango@thumbtribe.mobi>
2026-02-26 12:55:35 +0200

    Adding another chart to home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,7 +3,7 @@ import { TransactionTableComponent } from '@/app/features/transactions';
 import { TransactionModel } from '@/app/core/models';
 import { ChartComponent } from '@/app/shared';
 import { TransactionStore } from '../transactions/store/transaction.store';
-
+import { SpendByMonthModel } from '@/app/core/models/spendByMonth.model';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +11,7 @@ import { TransactionStore } from '../transactions/store/transaction.store';
   imports: [TransactionTableComponent, ChartComponent],
 })
 export class HomeComponent {
+  protected spendByMonthData = signal<SpendByMonthModel[]>([]);
   protected readonly store = inject(TransactionStore);
 
   protected readonly transactionList = computed<TransactionModel[]>(
@@ -21,6 +22,10 @@ export class HomeComponent {
     return this.store.dashboardSummary.value();
   });
 
+  protected readonly monthlySummary = computed(() => {
+    return this.store.monthlySummary.value() ?? [];
+  });
+
   protected get sortedList() {
     return this.transactionList()
       .sort((a, b) => {
@@ -29,15 +34,38 @@ export class HomeComponent {
       .slice(0, 5);
   }
 
-  protected readonly chartLabel = computed(() => {
-    return this.store.dashboardSummary.value()?.spendByCategory.map(l => l.name) ?? []
-  })
+  protected readonly spendByCategoryLabels = computed(() => {
+    return this.store.dashboardSummary.value()?.spendByCategory.map((l) => l.name) ?? [];
+  });
 
-  protected readonly chartValues = computed(() => {
-    return this.store.dashboardSummary.value()?.spendByCategory.map(v => v.total) ?? []
-  })
+  protected readonly incomeVsExpenseLabels = computed(() => {
+    return this.store.monthlySummary.value()?.map((l) => l.month) ?? [];
+  });
+
+  protected readonly spendByCategoryValues = computed(() => {
+    return this.store.dashboardSummary.value()?.spendByCategory.map((v) => v.total) ?? [];
+  });
 
   protected chartTotals() {
     return this.summary()?.spendByCategory.map((t) => t.total);
+  }
+
+  get incomeVsExpenseChartData() {
+    return [
+      {
+        label: 'Income',
+        data: this.monthlySummary().map((d) => d.income),
+        backgroundColor: '#4ade80',
+      },
+      {
+        label: 'Expense',
+        data: this.monthlySummary().map((d) => d.expense),
+        backgroundColor: '#f87171',
+      },
+    ];
+  }
+
+  get groupByCategoryData() {
+    return [{ label: 'Spending', data: this.spendByCategoryValues() }];
   }
 }
