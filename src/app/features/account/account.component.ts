9048ef7ad69229665af1f51c21984c29bdd3f18d
saziso <saziso.khango@thumbtribe.mobi>
2026-03-13 09:32:53 +0200

    Adding account component

---
+++ src/app/features/account/account.component.ts
@@ -0,0 +1,40 @@
+import { AccountRequestModel } from '@/app/core/models';
+import { ToastService } from '@/app/core/Services';
+import { AccountStore } from '@/app/core/store';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ButtonComponent } from "@/app/shared";
+import { FormDirective } from '@/app/shared/directives/form';
+
+@Component({
+  selector: 'app-account',
+  imports: [ButtonComponent, FormsModule, FormDirective],
+  templateUrl: './account.component.html',
+})
+export class AccountAccount {
+  protected isLoading = signal(false);
+  private readonly toastService = inject(ToastService);
+  private readonly store = inject(AccountStore);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
+
+  protected readonly formValue = signal<any>({});
+
+  onSubmitAccount(form: NgForm) {
+    if (form.invalid) return;
+
+    this.isLoading.set(true);
+    const newAccount: AccountRequestModel = {
+      ...form.form.value,
+    };
+
+    this.store.createAccount(newAccount).subscribe({
+      next: () => {
+        this.toastService.add('Account created successfully', 'success', 3000);
+        this.router.navigate(['/home']);
+      },
+      complete: () => this.isLoading.set(false),
+    });
+  }
+}
