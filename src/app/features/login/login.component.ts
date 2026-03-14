2a4e0d7c43557b3f6486e90f20b6da5c91c4ebea
saziso <saziso.khango@thumbtribe.mobi>
2026-03-14 09:25:13 +0200

    Adding functionality for login and bug

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,17 +1,36 @@
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ButtonComponent } from "@/app/shared";
+import { AuthStore } from '@/app/core/store';
+import { UserLoginRequest } from '@/app/core/models';
+import { FormDirective } from '@/app/shared/directives/form';
+import { ToastService } from '@/app/shared/toast.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logn',
-  imports: [FormsModule, ButtonComponent],
+  imports: [FormsModule, ButtonComponent, FormDirective],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
   protected readonly isLoading = signal(false);
   protected readonly formValue = signal<any>({});
+  protected readonly store = inject(AuthStore);
+  protected readonly route = inject(Router)
+  protected readonly toastService = inject(ToastService)
 
-  onLoginUser(event: any) {
-    throw new Error('Method not implemented.');
+  onLoginUser(form: NgForm) {
+    this.isLoading.set(true)
+    const loginRequest: UserLoginRequest = {
+      ...form.form.value
+    }
+
+    this.store.login(loginRequest).subscribe({
+      next: () => {
+        this.toastService.add('Login Successful', 'success', 4000)
+        this.route.navigate(['/home']);
+      },
+      complete: () => this.isLoading.set(false)
+    })
   }
 }
