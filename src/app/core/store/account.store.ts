605fab826adf1c143e97a5f56336986efbb3a445
saziso <saziso.khango@thumbtribe.mobi>
2026-03-18 18:37:09 +0200

    Adding behaviour subject

--- src/app/core/store/account.store.ts
+++ src/app/core/store/account.store.ts
@@ -1,34 +1,42 @@
-import { inject, Injectable } from "@angular/core";
-import { AccountService } from "../Services";
-import { AccountRequestModel } from "../models";
-import { catchError, throwError } from "rxjs";
-import { ToastService } from "@/app/shared/toast.service";
+import { inject, Injectable } from '@angular/core';
+import { AccountService } from '../Services';
+import { AccountRequestModel } from '../models';
+import { BehaviorSubject, catchError, of, switchMap, throwError } from 'rxjs';
+import { ToastService } from '@/app/shared/toast.service';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 
-
-
-  @Injectable({
-    providedIn: 'root'
-  }
-  )
+@Injectable({
+  providedIn: 'root',
+})
 export class AccountStore {
-    private readonly service = inject(AccountService);
-    private readonly toastService = inject(ToastService);
-  
-    createAccount(request: Readonly<AccountRequestModel>) {
-      return this.service.createAccount(request).pipe(
-        catchError((error) => {
-          this.toastService.add('Error creating accounts', 'error', 3000);
-          return throwError(() => error);
-        })
-      );
-    }
+  private readonly service = inject(AccountService);
+  private readonly toastService = inject(ToastService);
 
-    getAccounts() {
-      return this.service.getAllAccounts().pipe(
-        catchError((error) => {
+  private readonly reload$ = new BehaviorSubject<void>(undefined);
+
+  private readonly accounts$ = this.reload$.pipe(
+    switchMap(() => this.service.getAllAccounts().pipe(
+      catchError((error) => {
+        if (error.status !== 403) {
           this.toastService.add('Error getting all accounts', 'error', 3000);
-          return throwError(() => error);
-        })
-      )
-    }
+        }
+        return throwError(() => error);
+      })
+    ))
+  )
+
+  public readonly accounts = toSignal(this.accounts$, {initialValue: []})
+
+  reloadAccounts() {
+    this.reload$.next();
+  }
+
+  createAccount(request: Readonly<AccountRequestModel>) {
+    return this.service.createAccount(request).pipe(
+      catchError((error) => {
+        this.toastService.add('Error creating accounts', 'error', 3000);
+        return of([]);
+      })
+    );
+  }
 }
